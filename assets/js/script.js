23e758885c2a8e073934ef99cae2667c0e386a84
Ecar Poggi <poggiecar@gmail.com>
2023-05-29 21:35:49 -0300

    Finalizados los arreglos en el HTML, CSS y JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+
 window.addEventListener("scroll", function() {
     var nav = document.getElementById("nav");
     if (window.pageYOffset > nav.offsetTop) {
@@ -11,3 +13,60 @@ window.addEventListener("scroll", function() {
     const elem = document.getElementById("nombre");
     elem.style.color = red;
   }
+
+  // obteniendo los datos del  formulario y los campos de entrada.
+const form = document.getElementById("formulario");
+const nombreInput = document.getElementById("nombre");
+const emailInput = document.getElementById("email");
+const telefonoInput = document.getElementById("number");
+const mensajeInput = document.getElementById("mensaje");
+
+// Evento del submit.
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); // Evitar el envío del formulario por defecto
+
+  // Validar los campos
+  if (nombreInput.value === "") {
+    alert("Por favor, ingrese su nombre.");
+    return;
+  }
+
+  if (emailInput.value === "") {
+    alert("Por favor, ingrese su correo electrónico.");
+    return;
+  }
+
+  if (telefonoInput.value === "") {
+    alert("Por favor, ingrese su número de teléfono.");
+    return;
+  }
+
+ 
+  enviarFormulario();
+});
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  
+  const formData = {
+    nombre: nombreInput.value,
+    email: emailInput.value,
+    telefono: telefonoInput.value,
+    mensaje: mensajeInput.value
+  };
+
+
+  console.log(formData);
+  form.reset();
+});
+
+
+function enviarFormulario() {
+ 
+  alert("Formulario enviado correctamente.");
+
+  
+}
+
+
+});
