cbfa2483b62a22bbf5766442aea0db20074a1705
rocio <rocioma324@gmail.com>
2023-06-14 09:50:18 -0500

    Cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,53 @@
-document.addEventListener("DOMContentLoaded", function() {
-
 window.addEventListener("scroll", function() {
-    var nav = document.getElementById("nav");
-    if (window.pageYOffset > nav.offsetTop) {
-      nav.classList.add("nav-scroll");
-    } else {
-      nav.classList.remove("nav-scroll");
-    }
-  });
-
-  function changeColor(newColor) {
-    const elem = document.getElementById("nombre");
-    elem.style.color = red;
+  var nav = document.getElementById("nav");
+  if (window.pageYOffset > nav.offsetTop) {
+    nav.classList.add("nav-scroll");
+  } else {
+    nav.classList.remove("nav-scroll");
   }
-
-  // obteniendo los datos del  formulario y los campos de entrada.
-const form = document.getElementById("formulario");
-const nombreInput = document.getElementById("nombre");
-const emailInput = document.getElementById("email");
-const telefonoInput = document.getElementById("number");
-const mensajeInput = document.getElementById("mensaje");
-
-// Evento del submit.
-form.addEventListener("submit", function(event) {
-  event.preventDefault(); // Evitar el envío del formulario por defecto
-
-  // Validar los campos
-  if (nombreInput.value === "") {
-    alert("Por favor, ingrese su nombre.");
-    return;
-  }
-
-  if (emailInput.value === "") {
-    alert("Por favor, ingrese su correo electrónico.");
-    return;
-  }
-
-  if (telefonoInput.value === "") {
-    alert("Por favor, ingrese su número de teléfono.");
-    return;
-  }
-
- 
-  enviarFormulario();
-});
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  
-  const formData = {
-    nombre: nombreInput.value,
-    email: emailInput.value,
-    telefono: telefonoInput.value,
-    mensaje: mensajeInput.value
-  };
-
-
-  console.log(formData);
-  form.reset();
 });
 
-
-function enviarFormulario() {
- 
-  alert("Formulario enviado correctamente.");
-
-  
+function changeColor(newColor) {
+  const elem = document.getElementById("nombre");
+  elem.style.color = red;
 }
 
 
-});
+  const nombre= document.getElementById('nombre');
+  const email = document.getElementById('email');
+  const number = document.getElementById('number');
+  const mensaje = document.getElementById('mensaje');
+  const error = document.getElementById('error');
+  error.style.color='orange';
+  
+  const form= document.getElementById('formulario');
+  form.addEventListener('submit',e=>{
+    e.preventDefault();
+    let mensajesError= []
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  
+    if(nombre.value===null || nombre.value ===''){
+      mensajesError.push('Ingresa tu nombre');
+    }
+  
+    if(!regexEmail.test(email.value)){
+      mensajesError.push('Ingrese un email válido');
+  
+    }
+    if(number.value===null || number.value ===''){
+      mensajesError.push('Ingresa tu numero de telefono');
+    }
+    if(mensaje.value===null || mensaje.value ===''){
+      mensajesError.push('Ingresa tu mensaje');
+    }
+    error.innerHTML = mensajesError.join(', <br> ');
+
+    if(mensajesError.length===0){
+      alert("Formulario enviado correctamente.");
+      nombre.value= '';
+      email.value= '';
+      number.value= '';
+      mensaje.value= '';
+    }
+  })  
+
